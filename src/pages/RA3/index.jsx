d18ec48f7df7afdadffe1c93a84207c3e846b080
GerritV <shigw5420@joyowo.com>
2019-07-23 15:45:12 +0800

    board改成双层循环

--- src/pages/RA3/index.jsx
+++ src/pages/RA3/index.jsx
@@ -1,55 +1,40 @@
-import React from "react";
-import "./style.scss";
+import React from 'react'
+import './style.scss'
 
 function Square(props) {
   return (
     <button
-      className="square"
+      className='square'
       onClick={() => {
-        props.onClick();
+        props.onClick()
       }}
     >
       {props.value}
     </button>
-  );
+  )
 }
 
 class Board extends React.Component {
   renderSquare(i) {
-    return (
-      <Square
-        value={this.props.squares[i]}
-        onClick={() => this.props.onClick(i)}
-      />
-    );
+    return <Square value={this.props.squares[i]} onClick={() => this.props.onClick(i)} />
   }
 
   render() {
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {[1, 2, 3].map((item, i) => (
+          <div className='board-row' key='i'>
+            {[item * 3 - 1, item * 3 - 2, item * 3 - 3].map(s => this.renderSquare(s))}
+          </div>
+        ))}
       </div>
-    );
+    )
   }
 }
 
 class RA3 extends React.Component {
   constructor() {
-    super();
+    super()
     this.state = {
       history: [
         {
@@ -60,23 +45,23 @@ class RA3 extends React.Component {
       ],
       stepNumber: 0,
       xIsNext: true
-    };
+    }
   }
   jumpTo(step) {
     this.setState({
       stepNumber: step,
       xIsNext: step % 2 === 0
-    });
+    })
   }
   handleClick(i) {
-    const history = this.state.history.slice(0, this.state.stepNumber + 1);
-    const current = history[history.length - 1];
-    const squares = current.squares.slice(0);
+    const history = this.state.history.slice(0, this.state.stepNumber + 1)
+    const current = history[history.length - 1]
+    const squares = current.squares.slice(0)
     if (caculatorWinner(squares) || squares[i]) {
-      return;
+      return
     }
 
-    squares[i] = this.state.xIsNext ? "X" : "O";
+    squares[i] = this.state.xIsNext ? 'X' : 'O'
 
     this.setState({
       history: history.concat([
@@ -88,22 +73,22 @@ class RA3 extends React.Component {
       ]),
       stepNumber: history.length,
       xIsNext: !this.state.xIsNext
-    });
+    })
   }
   render() {
-    const history = this.state.history;
-    const current = history[this.state.stepNumber];
-    const winner = caculatorWinner(current.squares);
+    const history = this.state.history
+    const current = history[this.state.stepNumber]
+    const winner = caculatorWinner(current.squares)
 
     const moves = history.map((step, move) => {
-      const desc = move ? "Go to move #" + move : "Go to game start";
+      const desc = move ? 'Go to move #' + move : 'Go to game start'
       let DescLabel = () => {
         if (current === step) {
-          return <strong>{desc}</strong>;
+          return <strong>{desc}</strong>
         } else {
-          return <span>{desc}</span>;
+          return <span>{desc}</span>
         }
-      };
+      }
       return (
         <li key={move}>
           <button onClick={() => this.jumpTo(move)}>
@@ -111,53 +96,44 @@ class RA3 extends React.Component {
             坐标：（{step.x}, {step.y}）
           </button>
         </li>
-      );
-    });
+      )
+    })
 
-    let status;
+    let status
     if (winner) {
-      status = "Winner: " + winner;
+      status = 'Winner: ' + winner
     } else {
-      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O')
     }
     return (
-      <div className="game">
-        <div className="game-board">
+      <div className='game'>
+        <div className='game-board'>
           <Board
             squares={current.squares}
             onClick={i => {
-              this.handleClick(i);
+              this.handleClick(i)
             }}
           />
         </div>
-        <div className="game-info">
+        <div className='game-info'>
           <div>{status}</div>
           <ol>{moves}</ol>
         </div>
       </div>
-    );
+    )
   }
 }
 
 function caculatorWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
+  const lines = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
 
   for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
+    const [a, b, c] = lines[i]
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a].val;
+      return squares[a].val
     }
   }
-  return null;
+  return null
 }
 
-export default RA3;
+export default RA3
